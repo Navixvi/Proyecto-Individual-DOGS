58d1886f3ccb721a023501af2c09705310f46653
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2023-12-28 17:58:10 -0300

    DB

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,9 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
-
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+const { Dog , Temperaments } = sequelize.models;
+Dog.belongsToMany(Temperament, { through: 'DogsTemperaments' });
+Temperaments.belongsToMany(Dog, { through: 'DogsTemperaments' });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,27 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
-    name: {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    imagen: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    altura: {
+      type: DataTypes.FLOAT,
+    },
+    peso: {
+      type: DataTypes.FLOAT,
+    },
+    anosDeVida: {
+      type: DataTypes.INTEGER,
+    },
+  }, { timestamps: false });
 };
